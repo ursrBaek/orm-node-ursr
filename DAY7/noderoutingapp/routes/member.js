7f9b95750bfdff3a8252e8c2bb773e8a3b5a6295
ursrBaek <ursrbaek@gmail.com>
2023-12-07 12:13:07 +0900

    유효성검사 로직 수정 및 request객체 확인

--- DAY7/noderoutingapp/routes/member.js
+++ DAY7/noderoutingapp/routes/member.js
@@ -3,20 +3,34 @@ var router = express.Router();
 
 // 약관동의
 router.get('/join', function (req, res) {
-  
   res.render('member/join.ejs');
-})
+});
 
 // 회원가입
 router.get('/entry', function (req, res) {
-  
   res.render('member/entry.ejs');
-})
+});
 
 // 회원가입 처리
 router.post('/entry', function (req, res) {
-  
-  res.redirect('/main');
-})
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
+  const tel = req.body.tel;
+
+  const data = {
+    email,
+    password,
+    name,
+    tel,
+    entryDate: Date.now(),
+  };
+
+  console.log('--------------------------');
+  console.log('     클라이언트 입력 데이터:', data);
+  console.log('--------------------------');
+
+  res.redirect('/auth/login');
+});
 
 module.exports = router;
