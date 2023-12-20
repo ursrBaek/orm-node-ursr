3c756ae28bb1cf48c983fe69e1dfe63212d6afa4
ursrBaek <ursrbaek@gmail.com>
2023-12-20 14:58:00 +0900

    feat: express-ejs-layout 사용하여 템플릿 중복 제거

--- DAY16/nodeadminapp/app.js
+++ DAY16/nodeadminapp/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressLayouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,6 +18,13 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//레이아웃 설정
+app.set('layout', 'layout'); // 해당 노드앱의 모든 콘텐츠 뷰파일의 기본 레이아웃ejs파일 설정하기
+app.set('layout extractScripts', true); // 콘텐츠 페이지내 script 태그를 레이아웃에 통합할지 여부
+app.set('layout extractStyles', true); // 콘텐츠 페이지내 style 태그를 레이아웃에 통합할지 여부
+app.set('layout extractMetas', true); // 콘텐츠 페이지내 meta 태그를 레이아웃에 통합할지 여부
+app.use(expressLayouts);
 
 app.use(logger('dev'));
 app.use(express.json());
