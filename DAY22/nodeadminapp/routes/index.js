33bf36a833537d17ea846e5e85c8ab4f5a864181
ursrBaek <ursrbaek@gmail.com>
2023-12-29 00:57:38 +0900

    feat: index라우터 db객체 참조 및 로그인 처리

--- DAY22/nodeadminapp/routes/index.js
+++ DAY22/nodeadminapp/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// Model영역에서 db객체 참조하기
+var db = require('../models/index');
+
 /* GET home page. */
 
 // 메인 페이지 요청 및 응답
@@ -10,11 +13,18 @@ router.get('/', function (req, res, next) {
 
 // 로그인 웹페이지 요청 및 응답
 router.get('/login', function (req, res, next) {
-  res.render('login');
+  res.render('login', { resultMsg: '', email: '', password: '', layout: false });
 });
 
-router.post('/login', function (req, res, next) {
-  const { id, password } = req.body;
-  res.redirect('/');
+router.post('/login', async function (req, res, next) {
+  const { email, password } = req.body;
+
+  const admin_member = await db.Admin.findOne({ where: { email } });
+
+  if (admin_member?.admin_password === password) {
+    res.redirect('/');
+  } else {
+    res.render('login', { resultMsg: '이메일 또는 비밀번호를 잘못 입력하셨습니다.', email, password, layout: false });
+  }
 });
 module.exports = router;
