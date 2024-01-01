679cb66837164af2876141212863c625e34dd13e
ursrBaek <ursrbaek@gmail.com>
2024-01-01 23:48:30 +0900

    feat: member영역 DB연결 및 데이터 처리 로직 추가

--- DAY22/nodeadminapp/routes/member.js
+++ DAY22/nodeadminapp/routes/member.js
@@ -1,93 +1,93 @@
 var express = require('express');
 var router = express.Router();
-
-const memberList = [
-  {
-    member_id: 1,
-    member_password: '1234',
-    email: 'momo@naver.com',
-    name: '모모',
-    telephone: '010131311',
-    birth_date: '1991.11.11',
-    use_state_code: 1,
-    reg_date: '2021-11-22',
-  },
-  {
-    member_id: 2,
-    member_password: '1234',
-    email: 'jojo@naver.com',
-    name: '조조',
-    telephone: '010222222',
-    birth_date: '1991.11.12',
-    use_state_code: 0,
-    reg_date: '2021-11-23',
-  },
-  {
-    member_id: 3,
-    member_password: '1234',
-    email: 'toto@naver.com',
-    name: '토토',
-    telephone: '010333333',
-    birth_date: '1991.11.14',
-    use_state_code: 1,
-    reg_date: '2021-11-24',
-  },
-];
+var db = require('../models');
 
 /* GET home page. */
-router.get('/list', function (req, res, next) {
+router.get('/list', async function (req, res, next) {
+  const memberList = await db.Member.findAll();
   res.render('member/list', { memberList, searchOption: {} });
 });
 
-router.post('/list', function (req, res, next) {
+router.post('/list', async function (req, res, next) {
   const { name, email, telephone } = req.body;
   const searchOption = { name, email, telephone };
-  const memberList = [
-    {
-      member_id: 1,
-      member_password: '1234',
-      email: 'momo@naver.com',
-      name: '모모',
-      telephone: '010131311',
-      birth_date: '1991.11.11',
-      use_state_code: 1,
-      reg_date: '2021-11-22',
-    },
-  ];
+
+  const queryOptionObj = {};
+
+  for (let key in searchOption) {
+    if (searchOption[key]) {
+      queryOptionObj[key] = searchOption[key];
+    }
+  }
+
+  const memberList = await db.Member.findAll({ where: queryOptionObj });
+
   res.render('member/list', { memberList, searchOption });
 });
 
 router.get('/create', function (req, res, next) {
-  res.render('member/create', { title: 'Express' });
+  res.render('member/create', {});
 });
 
-router.post('/create', function (req, res, next) {
+router.post('/create', async function (req, res, next) {
+  const { email, member_password, name, profile_img_path, telephone, entry_type_code, use_state_code, birth_date } =
+    req.body;
+
+  const newMember = {
+    email,
+    member_password,
+    name,
+    profile_img_path,
+    telephone,
+    entry_type_code,
+    use_state_code,
+    birth_date,
+    reg_date: Date.now(),
+    reg_member_id: 2,
+  };
+
+  await db.Member.create(newMember);
+
   res.redirect('/member/list');
 });
 
-router.get('/modify/:mid', function (req, res, next) {
-  const memberIdx = req.params.mid;
+router.get('/modify/:mid', async function (req, res, next) {
+  const member_id = req.params.mid;
 
-  const member = {
-    member_id: 1,
-    member_password: '1234',
-    email: 'momo@naver.com',
-    name: '모모',
-    telephone: '010131311',
-    birth_date: '1991.11.11',
-    use_state_code: 1,
-    reg_date: '2021-11-22',
-  };
+  const member = await db.Member.findOne({ where: { member_id } });
 
   res.render('member/modify', { member });
 });
 
-router.post('/modify/:mid', function (req, res, next) {
-  const memberIdx = req.params.mid;
+router.post('/modify/:mid', async function (req, res, next) {
+  const member_id = req.params.mid;
+
+  const { email, member_password, name, profile_img_path, telephone, entry_type_code, use_state_code, birth_date } =
+    req.body;
+
+  const member = {
+    email,
+    member_password,
+    name,
+    profile_img_path,
+    telephone,
+    entry_type_code,
+    use_state_code,
+    birth_date,
+    edit_member_id: 20,
+    edit_date: Date.now(),
+  };
+
+  await db.Member.update(member, { where: { member_id } });
+
   res.redirect('/member/list');
 });
 
-router.get('/delete', function (req, res, next) {
+router.get('/delete', async function (req, res, next) {
+  const member_id = req.query.mid;
+
+  await db.Member.destroy({ where: { member_id } });
+
   res.redirect('/member/list');
 });
 
