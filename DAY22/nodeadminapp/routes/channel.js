f66125416683dc3069b3598c30291f1db87fac65
ursrBaek <ursrbaek@gmail.com>
2024-01-02 21:02:00 +0900

    feat: channel 영역 DB 연동 및 view 수정

--- DAY22/nodeadminapp/routes/channel.js
+++ DAY22/nodeadminapp/routes/channel.js
@@ -3,82 +3,34 @@
 var express = require('express');
 var router = express.Router();
 
-const channelList = [
-  {
-    channel_id: 1,
-    community_id: 2,
-    channel_code: '친목',
-    channel_name: '자바스크립트 스터디',
-    channel_img_path: '/image/img.jpg',
-    channel_desc: '자바스크립트 스터디 채팅방',
-    channel_state_code: 1,
-    reg_date: '2023-01-04',
-    user_limit: 50,
-    reg_member_id: 16,
-    edit_date: '2023-01-10',
-    edit_member_id: 16,
-  },
-  {
-    channel_id: 2,
-    community_id: 2,
-    channel_code: '공유',
-    channel_name: '거지방',
-    channel_img_path: '/image/img2.jpg',
-    channel_desc: '알뜰한 지출을 공유하는 방',
-    channel_state_code: 1,
-    user_limit: 60,
-    reg_date: '2020-01-04',
-    reg_member_id: 14,
-    edit_date: '2020-01-10',
-    edit_member_id: 14,
-  },
-  {
-    channel_id: 3,
-    community_id: 1,
-    channel_code: '친목',
-    channel_name: '삼합회',
-    channel_img_path: '/image/img.jpg',
-    channel_desc: '전국을 다스리는 삼합회 채팅방',
-    channel_state_code: 0,
-    user_limit: 100,
-    reg_date: '2020-01-02',
-    reg_member_id: 1,
-    edit_date: '2020-01-05',
-    edit_member_id: 1,
-  },
-];
+var db = require('../models');
 
-router.get('/list', function (req, res, next) {
+router.get('/list', async function (req, res, next) {
+  const channelList = await db.Channel.findAll();
+
   res.render('channel/list', { channelList, searchOption: {} });
 });
 
-router.post('/list', function (req, res, next) {
+router.post('/list', async function (req, res, next) {
   // step1: 사용자가 선택/입력한 조회옵션 데이터를 추출
   const { channel_name, reg_member_id, channel_state_code } = req.body;
 
   const searchOption = {
     channel_name,
     reg_member_id,
-    channel_state_code,
+    channel_state_code: channel_state_code === '9' ? '' : channel_state_code,
   };
 
-  // step2: 사용자가 입력/선택한 조회옵션 데이터를 기반으로 DB에서 게시글 목록 재조회 해오기
-  const channelList = [
-    {
-      channel_id: 3,
-      community_id: 1,
-      channel_code: '친목',
-      channel_name: '삼합회',
-      channel_img_path: '/image/img.jpg',
-      channel_desc: '전국을 다스리는 삼합회 채팅방',
-      channel_state_code: 0,
-      user_limit: 100,
-      reg_date: '2020-01-02',
-      reg_member_id: 1,
-      edit_date: '2020-01-05',
-      edit_member_id: 1,
-    },
-  ];
+  const queryOptionObj = {};
+
+  for (let key in searchOption) {
+    if (searchOption[key]) {
+      queryOptionObj[key] = searchOption[key];
+    }
+  }
+
+  const channelList = await db.Channel.findAll({ where: queryOptionObj });
+
   res.render('channel/list', { channelList, searchOption });
 });
 
@@ -86,37 +38,59 @@ router.get('/create', function (req, res, next) {
   res.render('channel/create');
 });
 
-router.post('/create', function (req, res, next) {
+router.post('/create', async function (req, res, next) {
+  const { community_id, category_code, channel_name, channel_state_code, user_limit, channel_desc } = req.body;
+
+  const newChannel = {
+    community_id,
+    category_code,
+    channel_name,
+    channel_state_code,
+    user_limit,
+    channel_desc,
+    channel_img_path: 'a',
+    reg_member_id: 100,
+    reg_date: Date.now(),
+  };
+
+  await db.Channel.create(newChannel);
   res.redirect('/channel/list');
 });
 
-router.get('/modify/:cid', function (req, res, next) {
-  const channelIdx = req.params.cid;
+router.get('/modify/:cid', async function (req, res, next) {
+  const channel_id = req.params.cid;
 
-  const channel = {
-    channel_id: 3,
-    community_id: 1,
-    channel_code: '친목',
-    channel_name: '삼합회',
-    channel_img_path: '/image/img.jpg',
-    channel_desc: '전국을 다스리는 삼합회 채팅방',
-    channel_state_code: 0,
-    user_limit: 100,
-    reg_date: '2020-01-02',
-    reg_member_id: 1,
-    edit_date: '2020-01-05',
-    edit_member_id: 1,
-  };
+  const channel = await db.Channel.findOne({ where: { channel_id } });
 
   res.render('channel/modify', { channel });
 });
 
-router.post('/modify', function (req, res, next) {
+router.post('/modify/:cid', async function (req, res, next) {
+  const channel_id = req.params.cid;
+
+  const { community_id, category_code, channel_name, channel_state_code, user_limit, channel_desc } = req.body;
+
+  const newChannel = {
+    community_id,
+    category_code,
+    channel_name,
+    channel_state_code,
+    user_limit,
+    channel_desc,
+    channel_img_path: 'a',
+    edit_member_id: 222,
+    edit_date: Date.now(),
+  };
+
+  await db.Channel.update(newChannel, { where: { channel_id } });
+
   res.redirect('/channel/list');
 });
 
-router.get('/delete', function (req, res, next) {
-  const articleIdx = req.query.cid;
+router.get('/delete', async function (req, res, next) {
+  const channel_id = req.query.cid;
+
+  await db.Channel.destroy({ where: { channel_id } });
 
   res.redirect('/channel/list');
 });
