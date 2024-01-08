426390a6cbf96eb284dd10f2c788438fa8e3ff92
ursrBaek <ursrbaek@gmail.com>
2024-01-08 12:41:40 +0900

    feat: 사용자 정보 관리 API 구현

--- DAY28/nodechatapp/routes/channel.js
+++ DAY28/nodechatapp/routes/channel.js
@@ -3,9 +3,6 @@
 var express = require('express');
 var router = express.Router();
 
-// Model영역에서 db객체 참조하기
-// var db = require('../models');
-
 // 채팅 웹페이지 GET 요청 - 채팅 웹페이지 응답;
 router.get('/', function (req, res, next) {
   res.render('chat', { layout: false });
--- DAY28/nodechatapp/routes/memberAPI.js
+++ DAY28/nodechatapp/routes/memberAPI.js
@@ -1,4 +1,5 @@
 // 회원 정보관리 RESTful API 전용 라우팅 기능 제공
+// http://localhost:3000/api/member/~
 
 var express = require('express');
 var router = express.Router();
@@ -14,7 +15,7 @@ router.get('/all', async function (req, res, next) {
   };
 
   try {
-    const memberList = await db.Member.findAll();
+    const memberList = await Member.find({});
     apiResult.code = 200;
     apiResult.data = memberList;
     apiResult.result = 'ok';
@@ -26,7 +27,74 @@ router.get('/all', async function (req, res, next) {
   res.json(apiResult);
 });
 
-// 신규 회원 정보 등록 처리 POST 요청 - 신규 회원 등록 처리
+router.post('/entry', async function (req, res, next) {
+  const apiResult = {
+    code: 200,
+    data: null,
+    result: '',
+  };
+
+  try {
+    const { name, member_password, telephone, email, birth_date } = req.body;
+
+    const newMember = {
+      name,
+      member_password,
+      telephone,
+      email,
+      profile_img_path: 'https://www.interpark.com/images/header/nav/icon_special.png',
+      entry_type_code: 1,
+      use_state_code: 1,
+      birth_date,
+      reg_date: Date.now(),
+      reg_member_id: 2,
+    };
+
+    const existingUser = await Member.findOne({ email });
+
+    if (existingUser) {
+      apiResult.code = 409;
+      apiResult.data = email;
+      apiResult.result = 'duplicatedEmail';
+    } else {
+      const member = await Member.create(newMember);
+
+      apiResult.code = 200;
+      apiResult.data = member;
+      apiResult.result = 'ok';
+    }
+  } catch (err) {
+    apiResult.code = 500;
+    apiResult.data = null;
+    apiResult.result = '서버에러발생 관리자에게 문의하세요.';
+  }
+
+  res.json(apiResult);
+});
+
+router.post('/login', async function (req, res) {
+  var apiResult = {
+    code: 200,
+    data: null,
+    result: '',
+  };
+  try {
+    const { email, member_password } = req.body;
+
+    const member = await Member.findOne({ email, member_password });
+
+    apiResult.code = 200;
+    apiResult.data = member;
+    apiResult.result = 'ok';
+  } catch (err) {
+    apiResult.code = 500;
+    apiResult.data = null;
+    apiResult.result = '서버에러발생 관리자에게 문의하세요.';
+  }
+
+  res.json(apiResult);
+});
+
 router.post('/create', async function (req, res, next) {
   const apiResult = {
     code: 200,
@@ -34,140 +102,132 @@ router.post('/create', async function (req, res, next) {
     result: '',
   };
 
-  // try {
-  //   const { name, member_password, telephone, email, birth_date } = req.body;
+  try {
+    const { name, member_password, telephone, email, birth_date } = req.body;
 
-  //   let birthDateStr = birth_date.split('-').join('').substr(2);
+    const newMember = {
+      name,
+      member_password,
+      telephone,
+      email,
+      profile_img_path: 'https://www.interpark.com/images/header/nav/icon_special.png',
+      entry_type_code: 1,
+      use_state_code: 1,
+      birth_date,
+      reg_date: Date.now(),
+      reg_member_id: 2,
+    };
 
-  //   const newMember = {
-  //     name,
-  //     member_password,
-  //     telephone,
-  //     email,
-  //     profile_img_path: 'https://www.interpark.com/images/header/nav/icon_special.png',
-  //     entry_type_code: 1,
-  //     use_state_code: 1,
-  //     birth_date: birthDateStr,
-  //     reg_date: Date.now(),
-  //     reg_member_id: 2,
-  //   };
+    const existingUser = await Member.findOne({ email });
 
-  //   const member = await db.Member.create(newMember);
+    if (existingUser) {
+      apiResult.code = 409;
+      apiResult.data = email;
+      apiResult.result = 'duplicatedEmail';
+    } else {
+      const member = await Member.create(newMember);
 
-  //   apiResult.code = 200;
-  //   apiResult.data = member;
-  //   apiResult.result = 'ok';
-  // } catch (err) {
-  //   apiResult.code = 500;
-  //   apiResult.data = null;
-  //   apiResult.result = '서버에러발생 관리자에게 문의하세요.';
-  // }
+      apiResult.code = 200;
+      apiResult.data = member;
+      apiResult.result = 'ok';
+    }
+  } catch (err) {
+    apiResult.code = 500;
+    apiResult.data = null;
+    apiResult.result = '서버에러발생 관리자에게 문의하세요.';
+  }
 
   res.json(apiResult);
 });
 
-router.post('/entry', async function (req, res, next) {
+router.post('/find', async function (req, res) {
   var apiResult = {
     code: 200,
     data: null,
     result: '',
   };
 
-  // try {
-  //   const { name, member_password, telephone, email, birth_date } = req.body;
+  try {
+    const { name, email, birth_date, telephone } = req.body;
 
-  //   let birthDateStr = birth_date.split('-').join('').substr(2);
+    const member = await Member.findOne({ name, email, birth_date, telephone });
 
-  //   const newMember = {
-  //     name,
-  //     member_password,
-  //     telephone,
-  //     email,
-  //     profile_img_path: 'https://www.interpark.com/images/header/nav/icon_special.png',
-  //     entry_type_code: 1,
-  //     use_state_code: 1,
-  //     birth_date: birthDateStr,
-  //     reg_date: Date.now(),
-  //     reg_member_id: 2,
-  //   };
-
-  //   const member = await db.Member.create(newMember);
-
-  //   apiResult.code = 200;
-  //   apiResult.data = member;
-  //   apiResult.result = 'ok';
-  // } catch (err) {
-  //   apiResult.code = 500;
-  //   apiResult.data = null;
-  //   apiResult.result = '서버에러발생 관리자에게 문의하세요.';
-  // }
+    if (member) {
+      apiResult.code = 200;
+      apiResult.data = member.member_password;
+      apiResult.result = 'ok';
+    } else {
+      apiResult.code = 404;
+      apiResult.data = null;
+      apiResult.result = '입력하신 회원정보가 존재하지 않습니다.';
+    }
+  } catch (err) {
+    apiResult.code = 500;
+    apiResult.data = null;
+    apiResult.result = '서버에러발생 관리자에게 문의하세요.';
+  }
 
   res.json(apiResult);
 });
 
 // 기존 회원 정보 데이터 수정처리 POST 요청 - 기존 회원 정보 데이터 수정처리
-router.post('/modify/:mid', async function (req, res, next) {
+router.post('/modify', async function (req, res, next) {
   const apiResult = {
     code: 200,
     data: [],
     result: 'ok',
   };
 
-  // try {
-  //   const member_id = req.params.mid;
+  try {
+    const member_id = req.query.mid;
 
-  //   const { name, member_password, telephone, email, birth_date } = req.body;
+    const { name, member_password, telephone, email, birth_date } = req.body;
 
-  //   let birthDateStr = birth_date.split('-').join('').substr(2);
+    const editedMember = {
+      name,
+      member_password,
+      telephone,
+      email,
+      birth_date,
+      edit_date: Date.now(),
+      edit_member_id: 3,
+    };
 
-  //   const editedMember = {
-  //     name,
-  //     member_password,
-  //     telephone,
-  //     email,
-  //     profile_img_path: 'https://www.interpark.com/images/header/nav/icon_special.png',
-  //     entry_type_code: 1,
-  //     use_state_code: 1,
-  //     birth_date: birthDateStr,
-  //     edit_date: Date.now(),
-  //     reg_member_id: 2,
-  //   };
+    const updateResult = await Member.updateOne({ member_id }, editedMember);
 
-  //   const updatedCount = await db.Member.update(editedMember, { where: member_id });
-
-  //   apiResult.code = 200;
-  //   apiResult.data = updatedCount;
-  //   apiResult.result = 'ok';
-  // } catch (err) {
-  //   apiResult.code = 500;
-  //   apiResult.data = null;
-  //   apiResult.result = 'Failed';
-  // }
+    apiResult.code = 200;
+    apiResult.data = updateResult.modifiedCount;
+    apiResult.result = 'ok';
+  } catch (err) {
+    apiResult.code = 500;
+    apiResult.data = null;
+    apiResult.result = 'Failed';
+  }
 
   res.json(apiResult);
 });
 
 // 기존회원 정보 데이터 삭제처리 POST 요청 - 기존 회원 정보 삭제 처리
-router.post('/delete', async function (req, res, next) {
+router.delete('/:mid', async function (req, res, next) {
   const apiResult = {
     code: 200,
     data: [],
     result: 'ok',
   };
 
-  // try {
-  //   const { member_id } = req.query.mid;
+  try {
+    const member_id = req.params.mid;
 
-  //   const deletedCnt = await db.Member.destroy({ where: member_id });
+    const deleteResult = await Member.deleteOne({ member_id });
 
-  //   apiResult.code = 200;
-  //   apiResult.data = deletedCnt;
-  //   apiResult.result = 'ok';
-  // } catch (err) {
-  //   apiResult.code = 500;
-  //   apiResult.data = null;
-  //   apiResult.result = 'Failed';
-  // }
+    apiResult.code = 200;
+    apiResult.data = deleteResult.deletedCount;
+    apiResult.result = 'ok';
+  } catch (err) {
+    apiResult.code = 500;
+    apiResult.data = null;
+    apiResult.result = 'Failed';
+  }
 
   res.json(apiResult);
 });
@@ -176,24 +236,25 @@ router.post('/delete', async function (req, res, next) {
 router.get('/:mid', async function (req, res, next) {
   const apiResult = {
     code: 200,
-    data: [],
+    data: null,
     result: 'ok',
   };
 
-  // try {
-  //   const member_id = req.params.mid;
+  try {
+    const member_id = req.params.mid;
 
-  //   const member = await db.Member.findOne({ where: { member_id } });
+    const member = await Member.findOne({ member_id });
 
-  //   apiResult.code = 200;
-  //   apiResult.data = member;
-  //   apiResult.result = 'ok';
-  // } catch (err) {
-  //   apiResult.code = 500;
-  //   apiResult.data = null;
-  //   apiResult.result = 'Failed';
-  // }
+    apiResult.code = 200;
+    apiResult.data = member;
+    apiResult.result = 'ok';
+  } catch (err) {
+    apiResult.code = 500;
+    apiResult.data = null;
+    apiResult.result = 'Failed';
+  }
 
   res.json(apiResult);
 });
+
 module.exports = router;
